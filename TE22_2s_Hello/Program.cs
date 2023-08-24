b0672cf8b2fcaf0efdafca3214ceef8ccff1ddd8
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-08-24 12:23:15 +0200

    Initial commit

---
+++ TE22_2s_Hello/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Hello, World!");
+
+string name = "Micke";
+
+Console.WriteLine($"Hej {name} vad snygg du är idag!");
+
+Console.ReadLine();
